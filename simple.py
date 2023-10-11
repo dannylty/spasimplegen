55c47ab0b0f35309a28d764c78627015cb0cc87c
Liu Tianyi <dannylty98@gmail.com>
2023-10-11 17:59:49 +0800

    Add assigns entity along with expression class updates

--- simple.py
+++ simple.py
@@ -1,4 +1,4 @@
-from typing import List
+import random
 
 from pkb import PKB
 pkb = PKB()
@@ -13,15 +13,24 @@ class Operator(Entity):
 
     def serialise(self, indent_level=0):
         return self.symbol
+    
+class BooleanOperator(Operator):
+    pass
 
 class Expression(Entity):
-    def __init__(self, left_term, operator, right_term):
+    def __init__(self, left_term, operator=None, right_term=None):
         self.left_term = left_term
         self.operator = operator
         self.right_term = right_term
 
     def serialise(self, indent_level=0):
-        return f"{self.left_term.serialise()} {self.operator} {self.right_term.serialise()}"
+        if self.operator is not None:
+            return f"{self.left_term.serialise()} {self.operator.serialise()} {self.right_term.serialise()}"
+        
+        return self.left_term.serialise()
+    
+class BooleanExpression(Expression):
+    pass
 
 class Term(Entity):
     def serialise(self, indent_level=0):
@@ -39,7 +48,14 @@ class Constant(Term):
         self.value = value
 
     def serialise(self, indent_level=0):
-        return self.value
+        return str(self.value)
+    
+class Bracket(Term):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def serialise(self, indent_level=0):
+        return f"({self.expression.serialise()})"
 
 class Procedure(Entity):
     def __init__(self, name, statements):
@@ -120,19 +136,68 @@ class WhileStatement(Statement):
             f"{indentation}}}"
         )
         return code
+    
+class AssignStatement(Statement):
+    def __init__(self, variable_name, expression):
+        self.variable_name = variable_name
+        self.expression = expression
 
-
-import random   
-APPEND_CHANCE = 0.7
-DECAY = 0.9999
-def roll():
-    global APPEND_CHANCE
-    APPEND_CHANCE *= DECAY
-    return APPEND_CHANCE
+    def serialise(self, indent_level=0):
+        indentation = " " * (indent_level * 4)
+        return f"{indentation}{self.variable_name} = {self.expression.serialise()};"
 
 class EntityFactory:
     def generate(self):
         pass
+
+class VariableFactory(EntityFactory):
+    def generate(self):
+        if ReadStatementFactory.variable_count == 0:
+            return Variable("var_1")
+        else:
+            variable_number = random.randint(1, ReadStatementFactory.variable_count)
+            return Variable(f"var_{variable_number}")
+
+class BracketFactory(EntityFactory):
+    def generate(self):
+        expression = ExpressionFactory().generate()
+        return Bracket(expression)
+    
+class TermFactory(EntityFactory):
+    def generate(self):
+        term_type = random.choice([ConstantFactory, VariableFactory, BracketFactory])
+        return term_type().generate()
+    
+class OperatorFactory(EntityFactory):
+    def generate(self):
+        operator = random.choice(['+', '-', '*'])
+        return Operator(operator)
+    
+class BooleanOperatorFactory(EntityFactory):
+    def generate(self):
+        symbol = random.choice(['==', '>', '<', '!='])
+        return BooleanOperator(symbol)
+
+class ConstantFactory(EntityFactory):
+    def generate(self):
+        return Constant(random.randint(1, 100))
+
+class ExpressionFactory(EntityFactory):
+    def generate(self):
+        if random.random() > TERM_CHANCE:
+            left_term = TermFactory().generate()
+            operator = OperatorFactory().generate()
+            right_term = TermFactory().generate()
+            return Expression(left_term, operator, right_term)
+        else:
+            return TermFactory().generate()
+        
+class BooleanExpressionFactory(EntityFactory):
+    def generate(self):
+        left_expr = ExpressionFactory().generate()
+        boolean_operator = BooleanOperatorFactory().generate()
+        right_expr = ExpressionFactory().generate()
+        return BooleanExpression(left_expr, boolean_operator, right_expr)
 
 class ProcedureFactory(EntityFactory):
     current_count = 0
@@ -156,10 +221,11 @@ class StatementListFactory(EntityFactory):
                     CallStatementFactory,
                     PrintStatementFactory,
                     ReadStatementFactory,
+                    AssignStatementFactory,
                     IfStatementFactory,
                     WhileStatementFactory
                  ],
-                weights = (20, 20, 20, 7, 7))[0]
+                weights = (20, 20, 20, 20, 7 * BRANCH_FACTOR, 7 * BRANCH_FACTOR))[0]
             statements.append(statement_factory().generate())
 
         return StatementList(statements)
@@ -197,35 +263,88 @@ class CallStatementFactory(EntityFactory):
 
 class PrintStatementFactory(EntityFactory):
     def generate(self):
-        variable_name = f"var_{random.randint(1, 100)}"
+        if ReadStatementFactory.variable_count == 0:
+            return ReadStatementFactory.generate()
+        
+        variable_name = f"var_{random.randint(1, ReadStatementFactory.variable_count)}"
         return PrintStatement(variable_name)
 
 class ReadStatementFactory(EntityFactory):
+    variable_count = 0
+
     def generate(self):
-        variable_name = f"var_{random.randint(1, 100)}"
+        ReadStatementFactory.variable_count += 1
+        variable_name = f"var_{ReadStatementFactory.variable_count}"
         return ReadStatement(variable_name)
-    
+
 class IfStatementFactory(EntityFactory):
     def generate(self):
-        condition = ExpressionGenerator().generate()
+        condition = BooleanExpressionFactory().generate()
         if_statements = StatementListFactory().generate()
         else_statements = StatementListFactory().generate()
         return IfStatement(condition, if_statements, else_statements)
     
 class WhileStatementFactory(EntityFactory):
     def generate(self):
-        condition = ExpressionGenerator().generate()
+        condition = BooleanExpressionFactory().generate()
         statements = StatementListFactory().generate()
         return WhileStatement(condition, statements)
 
-class ExpressionGenerator(EntityFactory):
+class AssignStatementFactory(EntityFactory):
     def generate(self):
-        return Expression(Variable('x'), '==', Constant('1'))
+        if ReadStatementFactory.variable_count == 0:
+            return ReadStatementFactory.generate()
+        
+        variable_name = f"var_{random.randint(1, ReadStatementFactory.variable_count)}"
+        expression = ExpressionFactory().generate()
+        return AssignStatement(variable_name, expression)
+
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="SIMPLE generator", epilog='e.g.: python3 simple.py -l 1.0 -b 0.1 -e 0.9')
+    
+    
+    parser.add_argument(
+        "-n",
+        type = int,
+        default = 5,  
+        help = "Number of procedures. Default 5."
+    )
+    parser.add_argument(
+        "-l",
+        type = float,
+        default = 0.8,
+        help = "Statement length factor. Higher value gives longer statement lists. Increasing this indirectly increases nesting depth. 0.0-1.0. Default 0.8."
+    )
+    parser.add_argument(
+        "-b",
+        type = float,
+        default = 1.0,
+        help = "Statement branch factor. 1x is the default probability for any statement to be a if/while. Anything beyond ~1.5 is ridiculous. Default 1.0."
+    )
+    parser.add_argument(
+        "-e",
+        type = float,
+        default = 0.5,
+        help = "Expression expansion probability. Higher value gives more branched expressions. 0.0-1.0. Default 0.5."
+    )
+
+    args = parser.parse_args()
+    
+    TERM_CHANCE = 1-args.e
+    APPEND_CHANCE = args.l
+    BRANCH_FACTOR = args.b
+    DECAY = 0.9999
+    def roll():
+        global APPEND_CHANCE
+        APPEND_CHANCE *= DECAY
+        return APPEND_CHANCE
+
+
     p = ProcedureFactory()
     buffer = ""
-    for _ in range(10):
+    for _ in range(args.n):
         buffer += p.generate().serialise()
         with open('generate.in', 'w') as f:
             f.write(buffer)
